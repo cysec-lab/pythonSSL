51c91131614efdea886c726db4adef0e4b193504
tomoya-shiraishi <33479219+tomoya-shiraishi@users.noreply.github.com>
2017-11-13 16:52:08 +0900

    1.2
    ループ

--- SSL3.6.py
+++ SSL3.6.py
@@ -1,35 +1,50 @@
 import socket
 import ssl
 import xlrd
+import xlwt
 
 x=0
 y=1
 z=2
-book=xlrd.open_workbook('lg.jp.xls')
-sheet = book.sheet_by_index(0)
-for row in range(1,15):
+n=1
+m=7
+bookr=xlrd.open_workbook('lg.jp.xls')
+bookw = xlwt.Workbook()
+sheet1 = bookw.add_sheet('test')
+sheet = bookr.sheet_by_index(0)
+for row in range(1,20):
      sock = socket.socket()
      ssl_sock = ssl.wrap_socket(sock)
      print(sheet.cell(row, 5).value)
      ssl_sock.connect((sheet.cell(row, 5).value, 443))
      der = ssl_sock.getpeercert(binary_form=True)
      print (ssl.DER_cert_to_PEM_cert(der))
-     f = open('証明書.der','w')
+     filename=sheet.cell(row, 5).value+'.der'
+     f = open(filename,'w')
      f.write(ssl.DER_cert_to_PEM_cert(der))
      f.close()
      list=ssl_sock.shared_ciphers()
+     
      for i in list:
-      lists=i    
+      lists=i
+  #sheet1_row = sheet1.row(n)
       print('暗号方式の名前',end='  ')
+     #sheet1_row.write(m,lists[x])
+      #m=m+1
       print(lists[x])  
       print('プロトコルのバージョン',end='  ')
+    #sheet1_row.write(m,lists[x])
+      #m=m+1
       print(lists[y])
       print('秘密鍵のビット長',end='  ')
+      #sheet1_row.write(m,lists[x])
       print(lists[z])
       print()
       x+3
       y+3
       z+3
+    #  n=n+1
+     # m=m+1
       
 
   
