fa54036eb63339e8da889e4e65c0c6dd86c83fd0
tomoya-shiraishi <33479219+tomoya-shiraishi@users.noreply.github.com>
2017-11-30 14:01:44 +0900

    1.4
    暗号方式の追加
    Excelへの書き込み(途中)
    go.jpのリスト追加

--- SSL3.6.py
+++ SSL3.6.py
@@ -4,21 +4,21 @@ import xlrd
 import xlwt
 from urllib.parse import urlparse
 
-
 x=0
 y=1
 z=2
-n=1
+n=0
 m=7
-ssl.OP_ALL
-bookr=xlrd.open_workbook('lg.jp.xls')
-bookw = xlwt.Workbook()
-sheet1 = bookw.add_sheet('test')
+bookr=xlrd.open_workbook('go.jp1.xlsx')
+#bookw = xlwt.Workbook()
+#newsheet_1 = bookw.add_sheet('NewSheet_1')
 sheet = bookr.sheet_by_index(0)
-for row in range(1,2):
+context = ssl.SSLContext()
+context.set_ciphers("AES:3DES:RSA:ECDHE:DH:ECDSA:CAMELLIA:DSS:SEED:SHA:RC4:MD5:PSK:GCM:CFB:OFB:CTR:ECB:IDEA")
+for row in range(1,50):
      sock = socket.socket()
-     ssl_sock = ssl.wrap_socket(sock)
-     o = urlparse(sheet.cell(row, 5).value)
+     ssl_sock = context.wrap_socket(sock)
+     o = urlparse(sheet.cell(row, 0).value)
      o=o.netloc
      print(o)
      ssl_sock.connect((o, 443))
@@ -29,25 +29,23 @@ for row in range(1,2):
      f.write(ssl.DER_cert_to_PEM_cert(der))
      f.close()
      list=ssl_sock.shared_ciphers()
+    # newsheet_1.write(n,0,o)
+     n=n+1
      
      for i in list:
       lists=i
-  #sheet1_row = sheet1.row(n)
-      print('暗号方式の名前',end='  ')
-     #sheet1_row.write(m,lists[x])
-      #m=m+1
-      print(lists[x])  
-      print('プロトコルのバージョン',end='  ')
-    #sheet1_row.write(m,lists[x])
-      #m=m+1
-      print(lists[y])
-      print('秘密鍵のビット長',end='  ')
-      #sheet1_row.write(m,lists[x])
+     # newsheet1_row = sheet1.row(n)
+      m=m+1
+      print(lists[x],end=' ')  
+     # newsheet1_row.write(m,lists[x])
+      m=m+1
+      print(lists[y],end=' ')
+     # newsheet1_row.write(m,lists[x])
       print(lists[z])
-      print()
+    #  book.save('gopro.xls')
       x+3
       y+3
       z+3
-    #  n=n+1
-     # m=m+1
+      n=n+1
+      m=m+1
 
